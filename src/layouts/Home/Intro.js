ff8778a9447e78409f9bfdc1b8ac5ab185ab0ba3
Chaitanya Sharma <chaitanya.sharma555@gmail.com>
2023-03-30 03:25:08 +0530

    v4

--- src/layouts/Home/Intro.js
+++ src/layouts/Home/Intro.js
@@ -77,7 +77,7 @@ export function Intro({ id, sectionRef, disciplines, scrollIndicatorHidden, ...r
                     data-status={status}
                     style={cssProps({ delay: tokens.base.durationXS })}
                   >
-                    Web Analyst
+                    Curious
                   </span>
                   <span className={styles.line} data-status={status} />
                 </span>
